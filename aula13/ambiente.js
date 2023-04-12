62d0aca38ea217cc2a00b1ec648a515155c9dce6
Emanuel Reis <emanuelemreis@gmail.com>
2023-04-12 11:58:39 -0300

    Repetição, exercício e prática

--- aula13/ambiente.js
+++ aula13/ambiente.js
@@ -1,5 +1,17 @@
-var c = 1
-while (c <= 6) {
+// var c = 1
+// while (c <= 6) { // Enquanto x, faça y
+//     console.log(`Passo ${c}`)
+//     c++
+// }
+
+// do { // Faça y enquanto x
+//     console.log(`Passo ${c}`)
+//     c++
+// } while (c <= 6)
+
+console.log('Vai começar!')
+for (var c = 1;c <= 7;c++) {
     console.log(`Passo ${c}`)
-    c++
 }
+console.log('Fim!')
+//Não esqueça de usar a depuração :D!!!
---
+++ praticas/contagem/script.js
@@ -0,0 +1,32 @@
+function contar() {
+    let ini = document.querySelector('input#ini')
+    let fim = document.querySelector('input#fim')
+    let passo = document.querySelector('input#pas')
+    let res = document.querySelector('p#res')
+    
+    if (ini.value.length <= 0 || fim.value.length <= 0 || passo.value.length <= 0) {
+        res.innerHTML = 'Impossível contar'
+        alert('[ERRO] Uma área não foi preenchida, tente novamente!')
+    } else {
+        let i = Number(ini.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+        res.innerHTML = 'Contando: '
+
+        if (p <= 0) {
+            alert('[ERR0] Passo Inválido! Considerado Passo 1')
+            p = 1
+        }
+
+        if (i < f) {
+            for (var c = i; c <= f; c += p) {
+                res.innerHTML += `${c} \u{1F449}`
+            }
+        } else {
+            for (var c = i; c >= f; c -=p) {
+                res.innerHTML += `${c} \u{1F449}`
+            }
+        }
+        res.innerHTML += `\u{1F3c1}`
+    }
+}
