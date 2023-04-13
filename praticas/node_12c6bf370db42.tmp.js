ffe974eba34685519849997707633d35dee63e69
Emanuel Reis <emanuelemreis@gmail.com>
2023-04-12 21:09:20 -0300

    Concertos e práticas

--- praticas/node_12c6bf370db42.tmp.js
+++
@@ -1,5 +0,0 @@
-var n1 = 0
-var cont = 1
-for (var soma = n1 + cont; cont <= 10; n1++) {
-    console.log(`${cont} + ${n1} = ${soma}`)
-}
--- praticas/p005.js
+++ praticas/p005.js
@@ -1,1 +1,16 @@
-console.log()
+var c = 1
+console.log('Vai comerçar!')
+// while (c <= 10 ) {
+//     console.log(`${c}`)
+//     c++
+// }
+
+// do {
+//     console.log(`${c}`)
+//     c++
+// } while (c <= 10)
+
+for (var c = 1; c <= 10; c++) {
+    console.log(`${c}`)
+}
+console.log('Fim!')
