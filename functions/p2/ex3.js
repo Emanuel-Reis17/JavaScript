cea9436eaf0afc530797d31f3881235bde3fcc13
Emanuel Reis <emanuelemreis@gmail.com>
2023-05-05 22:38:24 -0300

    tez

--- functions/p2/ex3.js
+++ functions/p2/ex3.js
@@ -1,5 +1,11 @@
-let somar = function(n){
-    return n * 2
+function validate(n){
+    if (n < 16){
+        console.log('Idade inválida')
+    } else if  (n == 16 || n < 18){
+        console.log('Estagiário')
+    } else if ( n >= 18){
+        console.log('APROVADO!')
+    }
 }
 
-console.log(somar(2))
+console.log(validate(26))
