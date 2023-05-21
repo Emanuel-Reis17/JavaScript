8d538622d88e796304920b8dad6e8f7197feaf6f
Emanuel Reis <emanuelemreis@gmail.com>
2023-05-21 12:37:50 -0300

    Verificador de idades

---
+++ praticas/idades/main.js
@@ -0,0 +1,33 @@
+function verificar(){
+    resultado = document.querySelector(".resultado");
+
+    //Colocando as informações dentro de um objeto
+    anos = {};
+    anos.nascimento = document.querySelector("#anoNascimento");
+    anos.atual = document.querySelector("#anoAtual");
+    
+    //Verificando erros nas informações
+    if (anos.nascimento.value.length == 0 || anos.atual.value.length == 0 || anos.nascimento.value > anos.atual.value){
+        alert("[ERRO] Por favor, preencha as informações!");
+    } else {
+        idade = Number(anos.atual.value - anos.nascimento.value)
+    }
+
+    //Iniciando a verificação
+    if(idade <= 20){
+        resultado.innerHTML = `Você tem ${idade} anos de idade, está na <strong>melhor fase da vida!</strong>`;
+    } else if(idade > 20 && idade <= 30){
+        resultado.innerHTML = `Você tem ${idade} anos de idade, está na <strong>fase adulta</strong>!`;
+    } else if(idade > 30 && idade <= 50){
+        resultado.innerHTML = `Você tem ${idade} anos de idade, está na <strong>fase madura</strong>!`;
+    } else if(idade > 50 && idade < 60){
+        resultado.innerHTML = `Você tem ${idade} anos de idade, está na fase de um(a) <strong>coroa</strong>!`;
+    } else if(idade >= 60 && idade <= 100){
+        resultado.innerHTML = `Você tem ${idade} anos de idade, está na <strong>fase de envelhecimento</strong>!`;
+    } else if(idade > 100 && idade <= 130){
+        resultado.innerHTML = `Você tem ${idade} anos de idade, parabéns por chegar nessa idade, você viveu muito, mas está muito próximo da <strong>morte</strong>.`;
+    } else {
+        alert("[ERRO] Idade Inexistente!, Por favor tente novament");
+        resultado.innerText = "[ERRO]"
+    }
+}
--- praticas/validacao/p011.js
+++ praticas/validacao/p011.js
@@ -1,6 +1,27 @@
 let user = {};
 let times = false;
 
+banco = function(){
+    opção = prompt("Que tipo dec conta deseja abrir? \n [1] Conata Poupança \n[2] Conta Corrente ");
+    let chances = false;
+
+    while(chances < 2){
+        if(opção == "1"){
+            user.account = prompt("Digite seu CPF: ");
+            if(user.account.value.length = 8){
+                alert("Ótimo! aguarde a análise de sua conta.");
+                break;
+            } else {
+                alert("[ERRO]");
+                chances++;
+            }
+        } else if (opção == "2"){
+            user.account = prompt("Digite seu CNPJ: ");
+            alert("Ótimo! aguarde a análise de sua conta.");
+        }
+    }
+}
+
 while(times < 3){
     user.name = prompt("Qual é o seu nome? ");
     user.email = prompt("Digite seu email: ");
@@ -8,6 +29,7 @@ while(times < 3){
     
     if(user.name == "emanuel" && user.email == "emanuel@gmail.com" && user.password == "415263"){
         alert(`Dados cadastrados! Seja muito bem-vindo senhor ${user.name}`);
+        banco();
         break;
     } else {
         alert("[ERRO]");
