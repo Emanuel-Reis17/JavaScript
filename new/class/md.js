2f088a456e893a81212cd74831f21c3d3826c489
Emanuel Reis <emanuelemreis@gmail.com>
2023-10-13 17:43:23 -0300

    Praticando

---
+++ new/class/md.js
@@ -0,0 +1,5 @@
+function sayHi() {
+    console.log("hi there!!");
+}
+
+export default sayHi;
--- new/class/test2.js
+++ new/class/test2.js
@@ -36,4 +36,50 @@ class Smartphone extends Dispositivo {
     }
 }
 
+class Tablet extends Dispositivo {
+    constructor(nome, modelo, cor, versao, tv, radio, infravermelho) {
+        super(nome, modelo);
+        this.cor = cor;
+        this.versao = versao;
+        this._tv = tv || false;
+        this._radio = radio || false;
+        this.infravermelho = infravermelho || false;
+        this[_preco]; 
+    }
+    get preco() {
+        console.log(`R$${this[_preco]}`);
+        return this[_preco];
+    }
+    set preco(valor) {
+        this[_preco] = valor;
+    }
+}
+
+class Computer extends Dispositivo {
+    constructor(nome, processador, placamae, fonte, ram, placaVideo) {
+        super("Computador");
+        this.processador = processador;
+        this.placaMae = placamae;
+        this.fonte = fonte;
+        this.ram = ram;
+        this.placaVideo = placaVideo || false;
+        this[_preco];
+    }
+    get preco() {
+        console.log(`R$${this[_preco]}`);
+    }
+    set preco(valor) {
+        this[_preco] = valor;
+    }
+}
+
 const s10 = new Smartphone("Samsung", "Galaxy S10", "Azul", 13);
+const ipadPro = new Tablet("Apple", "Ipad Pro", "Prata", 15);
+console.log(ipadPro);
+ipadPro.preco = 1000;
+console.log(ipadPro.preco);
+
+const pc1 = new Computer("Ryzen", "b550", 600, 16);
+console.log(pc1);
+pc1.preco = 800;
+pc1.preco;
